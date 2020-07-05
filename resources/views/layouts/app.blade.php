5624bba0b6c7a846edcf308ad76bea7cd8e3ea94
Remi <remi.brat@outlook.fr>
2020-07-05 23:38:48 +0200

    [ADD] profile buttons in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,18 @@
                         </span>
                     </div>
                 </form>
+
+                <ul class="navbar-nav ml-auto nav-flex-icons">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                          aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-user"></i>{{ Auth::user()->name }}</a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                          <a class="dropdown-item" href="#">My account</a>
+                          <a class="dropdown-item" href="#">Log out</a>
+                        </div>
+                      </li>
+                  </ul>
             </div>
 
         </nav>
